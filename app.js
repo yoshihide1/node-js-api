4dd9511fba6f7fe59810e5efa7bbb2ef6a0eb5dc
yoshihide <aqua.sky.33333@gmail.com>
2020-07-12 21:14:12 +0900

    node-cron追加

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express')
 const app = express()
 const mysql = require('mysql')
 const connection = require('./mysqlConnection')
+const cron = require('node-cron')
 
 server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
@@ -11,61 +12,69 @@ server = http.createServer((req, res) => {
   res.end()
 })
 
-//APIリクエスト
-let data = ""
-let pref = ""
-const url = 'https://covid19-japan-web-api.now.sh/api/v1/prefectures'
-const req = https.request(url, (res) => {
-  res.on('data', function(chunk) {
-     data += chunk 
-  })
-  res.on('end', () => {
-    pref = JSON.parse(data)
-     
-    for (let i in pref) {
-      let data = pref[i]
-      let prefData = {
-        id: data.id,
-        name: data.name_ja,
-        population: data.population,
-        cases: data.cases,
-        deaths: data.deaths,
-        pcr: data.pcr,
-        hospitalize: data.hospitalize,
-        severe: data.severe,
-        discharge: data.discharge,
-        lastUpdated: data.last_updated.cases_date
+//APIリクエスト定時実行
+
+  let data = ""
+  let pref = ""
+  const url = 'https://covid19-japan-web-api.now.sh/api/v1/prefectures'
+  const req = https.request(url, (res) => {
+    res.on('data', function (chunk) {
+      data += chunk
+    })
+    res.on('end', () => {
+      pref = JSON.parse(data)
+
+      for (let i in pref) {
+        let data = pref[i]
+        let prefData = {
+          pref_id: data.id,
+          population: data.population,
+          cases: data.cases,
+          deaths: data.deaths,
+          pcr: data.pcr,
+          hospitalize: data.hospitalize,
+          severe: data.severe,
+          discharge: data.discharge,
+          created_at: data.last_updated.cases_date
+        }
+
+        //データベースへの追加
+        connection.query(`insert into corona set ?`, prefData, (error, results, fields) => {
+          if (error) throw error
+          console.log("DB", [i])
+        })
       }
+    })
+  })
+
+  cron.schedule('57 23 * * *', () => {
+
+    req.on('error', (e) => {
+      console.error(`error:${e.message}`)
+    })
+    req.end()
+  })
+
+
+
+// データベースからの取得
+cron.schedule('57 23 * * *', () => {
+  console.log(11111)
+  console.log(3333)
+})
+// connection.query('select * from test1 where id = 13', ((error, results, fields) => {
+//   if (error) throw error
+//   // res.send(results)
+//   console.log(results)
+//   console.log("results:")
+// }))
 
 //       //削除
 //       // connection.query(`delete from test1 where id = ${i}`, (err,result) => {
 //       //   console.log("完了")
 //       // })
 
-//       //データベースへの追加
-      connection.query("insert into test2 set ?", prefData, (error, results, fields) => {
-        if (error) throw error
-        console.log("DB", [i])
-        
-      })
-    }
-    console.log('取得完了')
-  })
-})
 
-req.on('error', (e) => {
-  console.error(`error:${e.message}`)
-})
-req.end()
-
-  //データベースからの取得
-  // connection.query('select * from test1 where id = 13', ((error, results, fields) => {
-  //   if (error) throw error
-  //   // res.send(results)
-  //   console.log(results)
-  //   console.log("results:")
-
-  // }))
 
 app.listen(3000, (() => {
   console.log('listening on port 3000')

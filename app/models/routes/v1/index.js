f04b8f58d50fd845c66ce87cf19502fe4df9ffce
yoshihide <aqua.sky.33333@gmail.com>
2020-07-13 15:41:03 +0900

    appフォルダ削除

--- app/models/routes/v1/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-let router = express.Router()
-
-router.use('/article', require('./article.js'))
-router.use('/user', require('./user.js'))
-
-module.exports = router
-

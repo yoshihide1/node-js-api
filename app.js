535b9b4432ac43803f8f8119f1c7bd05b0bb09a3
yoshihide <aqua.sky.33333@gmail.com>
2020-07-16 09:31:00 +0900

    corsの設定追加

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const mysql = require('mysql')
 const connection = mysql.createConnection({
@@ -8,10 +9,22 @@ const connection = mysql.createConnection({
   password: process.env.password,
   database: process.env.database
 })
-//API部分
-app.get('/api/v1/', (req, res) => {//最新データ取得
 
+app.use(cors())
+//API部分
+app.get('/api/v1/today/', (req, res) => {//最新データ取得
   connection.query('select prefecture, cases, population, deaths, pcr, hospitalize, severe, discharge, created_at from corona join prefectures as pref on corona.pref_id = pref.pref_id where created_at = (select max(created_at) from corona)', (error, results, fields) => {
+    if (error) {
+      console.log(error)
+    } else {
+      res.json(results)
+    }
+  })
+})
+
+//日付事の集計
+app.get('/api/v1/total/', (req, res) => {
+  connection.query(`select date_format(created_at, '%Y-%m-%d') as date, sum(cases) as cases, sum(pcr) as pcr, sum(deaths) as deaths from corona group by date_format(created_at, '%Y%m%d') limit 14`, (error, results , fields) => {
     if (error) {
       console.log(error)
     } else {

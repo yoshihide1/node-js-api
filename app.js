2d0421ad71eef7ac577b06feb06324b8bced1403
yoshihide <aqua.sky.33333@gmail.com>
2020-07-24 12:11:29 +0900

    県別で取得できるAPI追加

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.get('/api/v1/2day/', (req, res) => {
 
 //県別
 app.get('/api/v1/pref/', (req, res) => {
-  connection.query(`select corona.*, pref.prefecture from corona join prefectures as pref on pref.pref_id = corona.pref_id and corona.pref_id =?`, [req.params.id], (error, results, fields) => {
+  connection.query(`select corona.*, pref.prefecture from corona join prefectures as pref on pref.pref_id = corona.pref_id and corona.pref_id =?`, [req.query.id], (error, results, fields) => {
     if (error) {
       console.log(error)
     } else {
